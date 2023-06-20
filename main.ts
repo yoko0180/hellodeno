527e5ca57b68a3aece147257ce6090e91c952aa9
yoko0180 <yoko0180@gmail.com>
2023-06-20 23:58:44 +0900

    update

--- main.ts
+++ main.ts
@@ -4,5 +4,5 @@ export function add(a: number, b: number): number {
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("*** hello deno6 ***")
+  console.log("*** hello deno7 ***")
 }
